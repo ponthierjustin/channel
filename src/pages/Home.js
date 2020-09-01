6aecd041fabbbb54475a3ec1eba802900e08d2bb
Justin <ponthierjustin@gmail.com>
2020-09-01 14:20:50 -0400

    styling of home page and png added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,24 +5,40 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
+import concert from "../images/unnamed.png";
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    fontFamily: "Ramabhadra",
-    color: "#eeeeee",
-    textAlign: "left",
+  topPaper: {
+    backgroundColor: "#222831",
+    height: theme.spacing(25),
   },
-  paperRight: {
-    backgroundColor: "#393e46",
+  paperText: {
     fontFamily: "Ramabhadra",
     color: "#eeeeee",
     textAlign: "center",
+    height: theme.spacing(10),
+    fontSize: "5.5rem",
+  },
+  middlePaper: {
+    backgroundColor: "#eeeeee",
+    alignItems: 'stretch'
+     /* height: theme.spacing(70),  */
   },
   paperLeft: {
     backgroundColor: "#eeeeee",
     fontFamily: "Ramabhadra",
     color: "#393e46",
     textAlign: "center",
+  },
+  paperRight: {
+    backgroundColor: "#393e46",
+    fontFamily: "Ramabhadra",
+    color: "#eeeeee",
+    textAlign: "center",
+    height: theme.spacing(70),
+  },
+  img: {
+    width: "100%",
   },
 }));
 
@@ -32,20 +48,21 @@ const Home = () => {
     <div>
       <Nav />
       <Grid container spacing={0}>
-        <Grid item xs={12} sm={12}>
-          <Typography className={classes.paper} variant="h1">
+        <Grid item xs={6} sm={6}>
+          <Paper className={classes.topPaper} elevation={0}></Paper>
+          <Typography xs={6} className={classes.paperText} variant="h1">
             CHANNEL
           </Typography>
         </Grid>
-        <Grid className={classes.paperLeft} item xs={7} sm={7}>
-          <Typography >
+        <Grid item xs={6} sm={6}></Grid>
+        <Grid className={classes.middlePaper} item xs={12} sm={5}>
+          <Typography className={classes.paperLeft}>
             FIND YOUR NEXT FAVORITE ARTIST
           </Typography>
+          <img className={classes.img} alt="concert" src={concert} />
         </Grid>
-        <Grid className={classes.paperRight} item xs={5} sm={5}>
-          <Typography >
-            THIS IS WERE i WILL DISPLAY CHARTS
-          </Typography>
+        <Grid className={classes.paperRight} item xs={12} sm={7}>
+          <Typography>THIS IS WERE i WILL DISPLAY CHARTS</Typography>
         </Grid>
       </Grid>
     </div>
