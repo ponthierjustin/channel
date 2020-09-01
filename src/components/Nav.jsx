f6474c00e8d5a55801917c4e3abc06979b0d7169
Justin <ponthierjustin@gmail.com>
2020-09-01 12:33:32 -0400

    added grid and styling for grid

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,33 +1,39 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import { makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    title: {
-      flexGrow: 1,
-      position: 'left',
-      fontFamily: 'Ramabhadra',
-      color: '#eeeeee'
-    },
-  }));
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: "left",
+    fontFamily: "Ramabhadra",
+    color: "#eeeeee",
+  },
+}));
 
 const Nav = () => {
   const classes = useStyles();
 
   return (
-  <div className={classes.root}>
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            CHANNEL
-          </Typography>
-          <Button color="inherit">SEARCH</Button>
-        </Toolbar>
-     
+    <div className={classes.root}>
+      <Toolbar>
+        <Typography variant="h4" className={classes.title}>
+          CHANNEL
+        </Typography>
+        <form noValidate autoComplete="off">
+          <TextField
+          label="Search"
+          id="outlined-size-small"
+          variant="outlined"
+          size="small"
+        />
+        </form>
+      </Toolbar>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,55 @@
-import React from 'react';
-import Nav from '../components/Nav';
+import React from "react";
+import Nav from "../components/Nav";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    fontFamily: "Ramabhadra",
+    color: "#eeeeee",
+    textAlign: "left",
+  },
+  paperRight: {
+    backgroundColor: "#393e46",
+    fontFamily: "Ramabhadra",
+    color: "#eeeeee",
+    textAlign: "center",
+  },
+  paperLeft: {
+    backgroundColor: "#eeeeee",
+    fontFamily: "Ramabhadra",
+    color: "#393e46",
+    textAlign: "center",
+  },
+}));
 
 const Home = () => {
-    return (
-        <div>
-            <Nav/>
-            <h1>HELLO</h1>
-        </div>
-    );
+  const classes = useStyles();
+  return (
+    <div>
+      <Nav />
+      <Grid container spacing={0}>
+        <Grid item xs={12} sm={12}>
+          <Typography className={classes.paper} variant="h1">
+            CHANNEL
+          </Typography>
+        </Grid>
+        <Grid className={classes.paperLeft} item xs={7} sm={7}>
+          <Typography >
+            FIND YOUR NEXT FAVORITE ARTIST
+          </Typography>
+        </Grid>
+        <Grid className={classes.paperRight} item xs={5} sm={5}>
+          <Typography >
+            THIS IS WERE i WILL DISPLAY CHARTS
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Home;
